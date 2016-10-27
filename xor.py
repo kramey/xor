2e5340afe1dd76804a013144567f9f1288389bff
Keith Ramey <keith.ramey@gmail.com>
2016-10-27 18:22:02 +0000

    difference

--- xor.py
+++ xor.py
@@ -2,6 +2,7 @@ from PIL import Image
 from PIL import ImageChops
 
 im1 = Image.open("A.png")
+im2 = Image.open("C.png")
 
-newimage = ImageChops.invert(im1)
+newimage = ImageChops.difference(im1,im2)
 newimage.save("new.png")
